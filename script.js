b8395ae0631b63dc7cf8ef7a73ab443e23c79ebc
Victor Nani <123387218+AristarhMD@users.noreply.github.com>
2026-07-05 17:19:38 +0300

    Pack selection update
    If click outside of div selection, div close
    Choosing the pack is chaning the data in the input of from, and update the UI

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+const packContainer = document.querySelector("#drop-down-container");
 const packBtn = document.querySelector("#planTrigger");
+const labelInfo = document.querySelector(".label");
+const tagInfo = document.querySelector(".tag");
 const packList = document.querySelector("#planList");
 const arrowIcon = document.querySelector("#drop-down-icon");
+const packInput = document.querySelector("#plan");
+const availablePacks = document.querySelectorAll("li");
 
+// Making btn for choose pack working
 packBtn.addEventListener("click", () => {
   packList.classList.toggle("hidden");
   arrowIcon.classList.toggle("rotate-180");
+});
+
+// If click is outside of the pack-container close pack-container
+document.addEventListener("click", (e) => {
+  if (!packContainer.contains(e.target)) {
+    packList.classList.add("hidden");
+    arrowIcon.classList.remove("rotate-180");
+  }
+});
+
+availablePacks.forEach((pack) => {
+  pack.addEventListener("click", () => {
+    const { value, label, tag } = { ...pack.dataset };
+    packInput.value = value;
+    labelInfo.textContent = label;
+    tagInfo.textContent = tag || "";
+
+    //   Hide list
+    packList.classList.add("hidden");
+    arrowIcon.classList.remove("rotate-180");
+  });
 });
